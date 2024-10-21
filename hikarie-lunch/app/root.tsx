b62e5325c713ac4c9459f6245d23386f3f624b44
sn0wEasy <yel.bocsmile@gmail.com>
2024-10-22 02:07:05 +0900

    モバイル版ではヘッダーがスクロールするように修正

--- hikarie-lunch/app/root.tsx
+++ hikarie-lunch/app/root.tsx
@@ -52,7 +52,6 @@ export default function App() {
       </head>
       <body>
         <div className="flex flex-col min-h-screen">
-          <Header />
           {/* PC版 */}
           {RestaurantListForWeb(restaurants)}
           {/* モバイル版 */}
@@ -65,7 +64,7 @@ export default function App() {
   );
 }
 
-const Header = () => {
+const HeaderForWeb = () => {
   return (
     <div className="h-24 px-4 pt-4 pb-2 bg-gray-800 text-white">
       <NavLink to="/">
@@ -73,7 +72,19 @@ const Header = () => {
       </NavLink>
       <div className="flex justify-between">
         <div className="text-xs">ヒカリエで働くひとのための、いい感じの飲食店がわかるサイト。</div>
-        {/* <div className="text-xs">このサービスについて</div> */}
+      </div>
+    </div>
+  );
+};
+
+const HeaderForMobile = () => {
+  return (
+    <div className="h-24 px-4 pt-4 pb-2 bg-gray-800 text-white">
+      <NavLink to="/">
+        <div className="text-4xl pb-2">Hikarie Workers Lunch</div>
+      </NavLink>
+      <div className="flex justify-between">
+        <div className="text-xs">ヒカリエで働くひとのための、いい感じの飲食店がわかるサイト。</div>
       </div>
     </div>
   );
@@ -83,26 +94,28 @@ const RestaurantList = ({ restaurants, setIsDetailFlag, className = "" }: { rest
   const [clickedRestaurantId, setClickedRestaurantId] = useState<string>("");
 
   return (
-    <div className={`${className} px-4 pt-4 pb-2 bg-red-100`}>
-      <ul>
-        {restaurants.map((restaurant) => (
-          <li key={restaurant.id}>
-            <NavLink
-              className={({ isActive, isPending }) =>
-                isActive
-                  ? "active"
-                  : isPending
-                    ? "pending"
-                    : ""
-              }
-              to={`detail/${restaurant.id}`}
-            >
-              <RestaurantCard restaurant={restaurant} isClicked={clickedRestaurantId === restaurant.id} setClickedRestaurantId={setClickedRestaurantId} setIsDetailFlag={setIsDetailFlag} />
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className={`${className} px-4 pt-4 pb-2 bg-red-100`}>
+        <ul>
+          {restaurants.map((restaurant) => (
+            <li key={restaurant.id}>
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "active"
+                    : isPending
+                      ? "pending"
+                      : ""
+                }
+                to={`detail/${restaurant.id}`}
+              >
+                <RestaurantCard restaurant={restaurant} isClicked={clickedRestaurantId === restaurant.id} setClickedRestaurantId={setClickedRestaurantId} setIsDetailFlag={setIsDetailFlag} />
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
@@ -171,30 +184,35 @@ const RestaurantCard = ({ restaurant, isClicked, setClickedRestaurantId, setIsDe
 };
 
 function RestaurantListForMobile(isDetailPage: boolean, setIsDetailFlag: React.Dispatch<React.SetStateAction<boolean>>, restaurants: ({ readonly id: string; readonly placeId: string; readonly displayName: string; readonly thumbnailPhotoUrl: string; readonly rating: number; readonly userRatingCount: number; readonly purposeLunch: boolean; readonly purposeDinner: boolean; readonly purposeCafe: boolean; readonly openingDays: { readonly monday: boolean; readonly tuesday: boolean; readonly wednesday: boolean; readonly thursday: boolean; readonly friday: boolean; readonly saturday: boolean; readonly sunday: boolean; } & {}; } & {})[]) {
-  return <div className="block xl:hidden">
-    <div className="flex flex-1 flex-col">
-      {isDetailPage ? (
-        <>
-          <div className="py-2 bg-blue-100">
-            <button onClick={() => setIsDetailFlag(false)}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-              </svg>
-            </button>
-          </div>
-          <div className="w-full h-[calc(100vh-6rem)] px-4 pb-4 bg-blue-100 overflow-y-auto">
-            <Outlet />
-          </div>
-        </>
-      ) : (
-        <RestaurantList restaurants={restaurants} setIsDetailFlag={setIsDetailFlag} className="w-full h-[calc(100vh-6rem)] overflow-y-auto" />
-      )}
-    </div>
-  </div>;
+  return (
+    <>
+      <div className="block xl:hidden">
+        <HeaderForMobile />
+        <div className="flex flex-1 flex-col">
+          {isDetailPage ? (
+            <>
+              <div className="py-2 bg-blue-100">
+                <button onClick={() => setIsDetailFlag(false)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+                  </svg>
+                </button>
+              </div>
+              <div className="w-full h-[calc(100vh-6rem)] px-4 pb-4 bg-blue-100">
+                <Outlet />
+              </div>
+            </>
+          ) : (
+            <RestaurantList restaurants={restaurants} setIsDetailFlag={setIsDetailFlag} className="w-full h-[calc(100vh-6rem)]" />
+          )}
+        </div>
+      </div>
+    </>);
 }
 
 function RestaurantListForWeb(restaurants: ({ readonly id: string; readonly placeId: string; readonly displayName: string; readonly thumbnailPhotoUrl: string; readonly rating: number; readonly userRatingCount: number; readonly purposeLunch: boolean; readonly purposeDinner: boolean; readonly purposeCafe: boolean; readonly openingDays: { readonly monday: boolean; readonly tuesday: boolean; readonly wednesday: boolean; readonly thursday: boolean; readonly friday: boolean; readonly saturday: boolean; readonly sunday: boolean; } & {}; } & {})[]) {
   return <div className="hidden xl:block">
+    <HeaderForWeb />
     <div className="flex flex-1">
       <RestaurantList restaurants={restaurants} className="hidden xl:block xl:w-1/3 h-[calc(100vh-6rem)] overflow-y-auto" />
       <div className="w-full xl:w-2/3 h-[calc(100vh-6rem)] px-4 py-4 bg-blue-100 overflow-y-auto">
